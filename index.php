2cff33025ae42e623c37522ba2360a1390f1746c
向军 <2300071698@qq.com>
2016-08-07 17:12:28 +0800

    优化

--- index.php
+++ index.php
@@ -7,12 +7,6 @@
  * |    WeChat: aihoudun
  * | Copyright (c) 2012-2019, www.houdunwang.com. All Rights Reserved.
  * '-------------------------------------------------------------------*/
-$f='create table a (uid int(10));
-            alter table a add ucreatetime int(10);';
-$d = preg_split( '/\r|\n/i', $f );
-preg_match('/alter\s+table\s+(.+?)\s/',$d[1],$f);
-
-print_r($f);
 //开启调试模式
 define( 'DEBUG', TRUE );
 //引入框架
