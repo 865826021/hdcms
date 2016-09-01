7d72273e122041bd0a0cb8c47ee83dbc2277a9bf
向军 <2300071698@qq.com>
2016-09-01 12:12:39 +0800

    关闭debug

--- index.php
+++ index.php
@@ -8,6 +8,6 @@
  * | Copyright (c) 2012-2019, www.houdunwang.com. All Rights Reserved.
  * '-------------------------------------------------------------------*/
 //开启调试模式
-define( 'DEBUG', TRUE );
+//define( 'DEBUG', TRUE );
 //引入框架
 require 'hdphp/hdphp.php';
