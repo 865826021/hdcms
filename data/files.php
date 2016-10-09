7f8a10294c2f23770a0d7974867377ab8d230c10
向军 <2300071698@qq.com>
2016-10-10 06:09:58 +0800

    优化

--- data/files.php
+++ data/files.php
@@ -4,8 +4,12 @@ M	app/system/controller/Component.php
 M	app/system/controller/Site.php
 M	app/system/view/cloud/updateSql.php
 M	app/system/view/cloud/upgrade.php
+D	data/database.php
 M	data/files.php
+M	data/install.sql
+M	data/upgrade.php
 A	install.php
+M	install/install.php
 M	resource/view/system.php
 M	system/middleware/Initialize.php
 M	system/service/build/Site.php
