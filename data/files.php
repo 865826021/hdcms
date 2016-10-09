48c9d2a048cc048e7c5b746fcdfc711bc8de5343
向军 <2300071698@qq.com>
2016-10-10 05:42:08 +0800

    优化

---
+++ data/files.php
@@ -0,0 +1,12 @@
+A	app/home/controller/Entry.php
+M	app/system/controller/Cloud.php
+M	app/system/controller/Component.php
+M	app/system/controller/Site.php
+M	app/system/view/cloud/updateSql.php
+M	app/system/view/cloud/upgrade.php
+M	data/files.php
+A	install.php
+M	resource/view/system.php
+M	system/middleware/Initialize.php
+M	system/service/build/Site.php
+M	vendor/houdunwang/framework/src/dir/Dir.php
