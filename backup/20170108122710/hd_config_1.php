34361483a61323174afe0dc51e32dadf88f4496c
houdunwang <2300071698@qq.com>
2017-01-08 14:03:28 +0800

    no message

--- backup/20170108122710/hd_config_1.php
+++
@@ -1,6 +0,0 @@
-<?php 
-\houdunwang\db\Db::table('hd_config',true)->replace(array (
-  'id' => '1',
-  'site' => '{"is_open":"1","enable_code":"0","close_message":"网站维护中,请稍候访问","upload":{"size":20000,"type":"jpg,jpeg,gif,png,zip,rar,doc,txt,pem"}}',
-  'register' => '{"is_open":"0","audit":"1","enable_code":"0","groupid":"1"}',
-));
--- backup/20170108122710/hd_user_group_1.php
+++
@@ -1,8 +0,0 @@
-<?php 
-\houdunwang\db\Db::table('hd_user_group',true)->replace(array (
-  'id' => '1',
-  'name' => '体验用户组',
-  'maxsite' => '5',
-  'daylimit' => '7',
-  'package' => 's:0:"";',
-));
