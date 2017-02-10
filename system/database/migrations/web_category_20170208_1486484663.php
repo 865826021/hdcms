1ac86fcd7c20e1676856d4bfc909b598ff138266
houdunwang <2300071698@qq.com>
2017-02-11 06:32:23 +0800

    no message

--- system/database/migrations/web_category_20170208_1486484663.php
+++ system/database/migrations/web_category_20170208_1486484663.php
@@ -11,20 +11,18 @@ class web_category extends Migration {
 CREATE TABLE `hd_web_category` (
   `cid` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `siteid` int(10) unsigned NOT NULL,
+  `mid` int(11) unsigned NOT NULL COMMENT '模型编号',
   `title` varchar(100) NOT NULL COMMENT '栏目标题',
   `pid` int(10) unsigned NOT NULL COMMENT '父级编号',
   `orderby` tinyint(3) unsigned NOT NULL COMMENT '排序',
   `status` tinyint(1) NOT NULL COMMENT '状态',
   `icontype` tinyint(1) NOT NULL COMMENT '1 图标 2 图片',
   `description` varchar(255) NOT NULL COMMENT '栏目描述',
-  `template_name` varchar(50) NOT NULL DEFAULT '' COMMENT '模板',
   `linkurl` varchar(300) NOT NULL COMMENT '外部链接',
   `ishomepage` tinyint(1) unsigned NOT NULL COMMENT '封面栏目',
   `css` varchar(500) NOT NULL COMMENT 'css样式',
-  `web_id` int(11) NOT NULL COMMENT '选择添加到站点首页导航时的站点编号，只对微站首页导航有效',
   PRIMARY KEY (`cid`),
-  KEY `siteid` (`siteid`),
-  KEY `template_name` (`template_name`)
+  KEY `siteid` (`siteid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='文章分类';
 sql;
 		Db::execute( $sql );
