b97d96f680c69833d05c73f1d7fcbe798a2ae569
houdunwang <2300071698@qq.com>
2017-02-20 19:20:52 +0800

    no message

--- system/database/migrations/cache_20170208_1486484663.php
+++ system/database/migrations/cache_20170208_1486484663.php
@@ -10,13 +10,11 @@ class cache extends Migration {
 		}
 		$sql = <<<sql
 CREATE TABLE `hd_cache` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `name` char(20) NOT NULL DEFAULT '' COMMENT '标识名称',
   `data` mediumtext NOT NULL COMMENT '数据',
   `create_at` int(10) NOT NULL COMMENT '创建时间',
   `expire` int(10) NOT NULL COMMENT '过期时间',
   `siteid` int(11) NOT NULL COMMENT '站点编号 -1为系统配置0为无效 正数为站点编号',
-  PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
 sql;
