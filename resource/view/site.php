7d9ae5ef27d5b4f9cfffb5c99d561b53ce2f689b
houdunwang <2300071698@qq.com>
2017-02-24 17:35:05 +0800

    no message

--- resource/view/site.php
+++ resource/view/site.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8"/>
-	<title>HDCMS - 开源免费内容管理系统 - Powered by HDCMS.COM</title>
+	<title>{{v('site.info.name')}} - HDCMS开源免费内容管理系统</title>
 	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon"/>
 	<meta name="csrf-token" content="{{ csrf_token() }}">
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
