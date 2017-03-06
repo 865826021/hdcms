13e0cccaafcda5742d830ceb76ca0086d99f4b38
houdunwang <2300071698@qq.com>
2017-03-06 14:21:12 +0800

    no message

--- system/database/seeds/config_20170208_1486484663.php
+++ system/database/seeds/config_20170208_1486484663.php
@@ -1,17 +1,20 @@
 <?php namespace system\database\seeds;
-use houdunwang\database\build\Seeder;
-class config extends Seeder {
-    //执行
-	public function up() {
-		$sql=<<<str
-INSERT INTO `hd_config` (`id`, `site`, `register`)
-VALUES
-	(1,'{\"is_open\":\"1\",\"close_message\":\"网站维护中,请稍候访问\",\"enable_code\":1,\"upload\":{\"size\":\"2000\",\"type\":\"jpg,jpeg,gif,png,zip,rar,doc,txt,pem,json\",\"path\":\"attachment\"},\"way\":\"local\",\"http\":{\"rewrite\":\"1\"},\"app\":{\"debug\":\"1\"}}','{\"is_open\":\"1\",\"audit\":\"0\",\"enable_code\":\"0\",\"groupid\":\"1\"}');
-str;
-		Db::execute($sql);
-    }
-    //回滚
-    public function down() {
 
-    }
+use houdunwang\database\build\Seeder;
+
+class config extends Seeder {
+	//执行
+	public function up() {
+		$sql = <<<str
+INSERT INTO `hd_config` (`site`, `register`)
+VALUES
+	('{\"is_open\":\"1\",\"close_message\":\"网站维护中,请稍候访问\",\"enable_code\":0,\"upload\":{\"size\":\"2000\",\"type\":\"jpg,jpeg,gif,png,zip,rar,doc,txt,pem,json,mp4\",\"path\":\"attachment\",\"mold\":\"oss\"},\"way\":\"oss\",\"http\":{\"rewrite\":\"1\"},\"app\":{\"debug\":\"1\"},\"oss\":{\"accessKeyId\":\"LTAIPSSlOmIqFeo3\",\"accessKeySecret\":\"VA5HVmet9ioRRIxs9muZ2qQrpD1c6M\",\"endpoint\":\"http://oss-cn-hangzhou.aliyuncs.com\",\"bucket\":\"houdunwang\",\"bucket_endpoint\":\"http://houdunren.oss-cn-hangzhou.aliyuncs.com\"}}','{\"is_open\":\"1\",\"audit\":\"0\",\"enable_code\":\"0\",\"groupid\":\"1\"}');
+str;
+		Db::execute( $sql );
+	}
+
+	//回滚
+	public function down() {
+
+	}
 }
