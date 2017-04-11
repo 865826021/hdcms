32bce299c806443cc4fdc02f77fc5cd27da20a6c
houdunwang <2300071698@qq.com>
2017-04-11 12:10:42 +0800

    no message

--- system/database/migrations/a0415_20170408_1491583192.php
+++ system/database/migrations/a0415_20170408_1491583192.php
@@ -5,10 +5,7 @@ use houdunwang\database\build\Blueprint;
 class a0415 extends Migration {
     //执行
 	public function up() {
-		Schema::table('modules_bindings', function (Blueprint $table) {
-			//修改字段
-			$table->string('json', 2000)->comment('数据')->add();
-		});
+
     }
 
     //回滚
